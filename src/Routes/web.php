1c5ba6bc4253a76fc1eed5cb12178627810a7e0b
Sjoerd van Bergen <sjoerdvanbergen@MekMini.local>
2023-02-02 10:29:05 +0100

    fixing initial version

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-// Route::get('/', function () {
-//     return view('website.landing-page');
-// })->name('home');
+ Route::get('/rw_invoice_test', function () {
+     return 'haha';
+ })->name('home');
