56fdb538bf5f86d38ca662d1810fd5ccb65a7862
Sjoerd van Bergen <sjoerdvanbergen@MekMini.local>
2023-02-02 13:14:25 +0100

    fixing initial version

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -20,10 +20,12 @@ use Illuminate\Support\Facades\Auth;
  });
 
 
-Route::get('/rw_document_test', function ()
+Route::get('/rw_document_test', function () {
 
     $rockitDocument = \RockitDocument::init();
+
     dd($rockitDocument);
+
     return \RockitDocument::init()->createPDF();
 //        ->setViewData([
 //            'person' => [
