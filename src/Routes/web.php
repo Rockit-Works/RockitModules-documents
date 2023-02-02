aeef31c7d772bc90f75bd18cc365e032c1da86a7
Sjoerd van Bergen <sjoerdvanbergen@MekMini.local>
2023-02-02 13:17:48 +0100

    fixing initial version

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Auth;
  });
 
 
-Route::get('/rw_document_test', function ()
+Route::get('/rw_document_test', function () {
 
     return \RockitDocument::init()
         ->setViewData([
